c7aabf1b93ab558198a7ed81e0ca4058a4cb0cbb
Paul Jesukiewicz <pjesukiewicz@linus-public.highpoint.edu>
2019-03-25 17:51:48 -0400

    created seperate files for lex parsing and header file

---
+++ general.h
@@ -0,0 +1,12 @@
+#define LETTER 0
+#define DIGIT 1
+#define OPERATOR 99
+#define INT_LIT 10
+#define IDENT 11
+#define ASSIGN_OP 20
+#define ADD_OP 21
+#define SUB_OP 22
+#define MULT_OP 23
+#define DIV_OP 24
+#define LEFT_PAREN 25
+#define RIGHT_PAREN 26
