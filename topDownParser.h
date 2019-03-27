ea4f23cf0ce4577a5fc53991d69117ccd58afdf2
pauliejes <pauliejes@gmail.com>
2019-03-27 18:22:43 -0400

    Whoops, accidently undeclared expr()

--- topDownParser.h
+++ topDownParser.h
@@ -5,9 +5,9 @@
 #ifndef _TOP_DOWN_PARSER_H_
 #define _TOP_DOWN_PARSER_H_
 /* Function declarations */
+void expr();
 void term();
 void factor();
-void exponent();
 void error(const char *);
 
 #endif
