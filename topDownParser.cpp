d14ec5267f710249cd9079adcf2fd0bacea713b4
Ryan Dillin Felton <ryandillinfelton@gmail.com>
2019-03-29 10:27:39 -0400

    gosh!

--- topDownParser.cpp
+++ topDownParser.cpp
@@ -47,11 +47,10 @@ int main(void) {
    // if ((in_fp = fopen("front.in", "r")) == NULL)
    //    printf("ERROR - cannot open front.in \n");
    // else {
-  getChar();
   do {
+    getChar();
     lex();
-    stmt_list();
-    //expr();
+    cout << "    = " << stmt() << endl;
   } while (nextToken != EOF);
 
    //}
@@ -63,19 +62,19 @@ int main(void) {
    <stmt_list> -> <stmt_list> <stmt> NEWLINE
 */ 
 
-void stmt_list() 
+/*void stmt_list() 
 {
    printf("Enter <stmt_list>\n");
    //process statement
    cout << "       = " << stmt() << endl;
    while(nextToken == NEWLINE) {
-     cout << "loop\n";
+     //cout << "loop\n";
      getChar();
      lex();
      cout << "       = " << stmt() << endl;
    }
    printf("Exit <stmt_list>\n");
-}
+}*/
 
 
 /* stmt
@@ -87,30 +86,35 @@ void stmt_list()
 int stmt()
 {
    printf("Enter <stmt>\n");
-   int return_val;
+   int return_val = 0;
    //for assignment statements
    if(nextToken == IDENT) {
-      //get the next token
-      Symbol_ptr var_to_assign = symbolTable.insert(lexeme);
-      //expr is too extreme here, should only 
-      if(cin.peek() != '='){
-         return_val = expr();
+      //check for quit
+      if(strcmp(lexeme,"quit") == 0) {
+        exit(11);
+      }
+      else if(strcmp(lexeme,"dump") == 0) {
+        symbolTable.dump_table();
+        //lex();
+        //return 0;
       }
       else {
-         factor();
+         //get the next token
+         Symbol_ptr var_to_assign = symbolTable.insert(lexeme);
+         //expr is too extreme here, should only 
+         return_val = expr();
+            //factor();
          while (nextToken == ASSIGN_OP) {
             lex();
             return_val = stmt();
             //return_val = expr();
-            cout << "return_val = " << return_val << endl;
+            //cout << "return_val = " << return_val << endl;
             var_to_assign->putval(return_val);
          }
       }
    } else {
       //plane expressions
-      cout << "else\n";
       return_val = expr();
-      cout << "ereturn_val = " << return_val << endl;
    }
    cout << "Exit <stmt>  return: " << return_val << endl;
    return return_val;
@@ -162,16 +166,29 @@ int term()
    printf("Enter <term>\n");
    /* Parse the first factor */
    if (nextToken == SUB_OP) {
+      //negative
       lex();
-      return_val = factor();
+      return_val = factor() * -1;
    } else {
       return_val = factor();
    }
    /* As long as the next token is * or /, get the
       next token and parse the next factor */
-   while (nextToken == MULT_OP || nextToken == DIV_OP || nextToken == MOD_OP || nextToken == POW_OP) {
+   while (nextToken == MULT_OP){
       lex();
-      return_val = factor();
+      return_val *= factor();
+   } 
+   while ( nextToken == DIV_OP) {
+      lex();
+      return_val /= factor();
+   } 
+   while (nextToken == MOD_OP) {
+      lex();
+      return_val = return_val % factor();
+   }
+   while (nextToken == POW_OP) {
+      lex();
+      return_val = pow(return_val,factor());
    }
 
    cout << "Exit <term>  return: " << return_val << endl;
@@ -187,15 +204,14 @@ int term()
  */
 int factor()
 {
-   int return_val;
+   int return_val = 0;
    printf("Enter <factor>\n");
    /* Determine which RHS */
    if (nextToken == IDENT) {
-      cout << "ident lexeme = " << lexeme << endl;
       //look up the ident in the symbol table
-      Symbol_ptr ident_node = symbolTable.insert(lexeme);
+      return_val = symbolTable.insert(lexeme)->getval();
+      cout << "ident lexeme = " << lexeme << " = " << return_val << endl;
       lex();
-      return_val = ident_node->getval();
    } else if (nextToken == INT_LIT){
       /* Get the next token d*/
       return_val = atoi(lexeme);
